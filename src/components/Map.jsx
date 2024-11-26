6d707d9ab0686da33bd0e7113e86524c099f906b
mugisha isaie <mugishaisaieog1@gmail.com>
2024-11-26 19:43:52 +0200

    postioning center map current location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,11 +12,15 @@ function Map() {
   const [mapPostion,setMapPostion] = useState([40,0])
 
   const [searchParams,setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat")
-  const lng = searchParams.get("lng")
+  const mapLat = searchParams.get("lat")
+  const mapLng = searchParams.get("lng")
   return (
-   <div className={styles.mapContainer} onClick={()=>navigate("form")}>
-    <MapContainer center={mapPostion} zoom={13} scrollWheelZoom={true} className={styles.map}>
+   <div className={styles.mapContainer}>
+    <MapContainer 
+    // center={mapPostion} 
+    center={[mapLat, mapLng]} 
+    
+    zoom={6} scrollWheelZoom={true} className={styles.map}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
